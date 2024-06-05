1b2ca089f5a372f6596d5e0a018770b7b468e33c
unknown <p6579113@gmail.com>
2024-06-05 22:37:38 +0530

    table

--- admin-panel pro/script-files/script.js
+++ admin-panel pro/script-files/script.js
@@ -177,6 +177,15 @@ var options = {
   chart.render();
 
 
+  let table = new DataTable('#example');
+ 
+table.on('click', 'tbody tr', function () {
+    let data = table.row(this).data();
+ 
+    alert('You clicked on ' + data[0] + "'s row");
+});
+
+
 
 
 
