ff2acd971e59a18e9c346b92a7f627a698182e61
unknown <p6579113@gmail.com>
2024-06-07 22:22:39 +0530

    last

--- admin-panel pro/script-files/script.js
+++ admin-panel pro/script-files/script.js
@@ -148,6 +148,18 @@ var options = {
   })
 
 
+  //table section
+  
+  let table = new DataTable('#example');
+ 
+table.on('click', 'tbody tr', function () {
+    let data = table.row(this).data();
+ 
+    alert('You clicked on ' + data[0] + "'s row");
+});
+  
+
+
 
 
 
